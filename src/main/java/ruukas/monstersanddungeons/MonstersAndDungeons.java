8a31cc3a12b02e283b69b3018f2a1c7bb86dccd8
Ruukas <lucas-olai@hotmail.com>
2017-11-01 20:20:26 +0100

    Init

---
+++ src/main/java/ruukas/monstersanddungeons/MonstersAndDungeons.java
@@ -0,0 +1,17 @@
+package ruukas.monstersanddungeons;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = MonstersAndDungeons.MODID, version = MonstersAndDungeons.VERSION, name = "Monsters and Dungeons")
+public class MonstersAndDungeons
+{
+    public static final String MODID = "monstersanddungeons";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
