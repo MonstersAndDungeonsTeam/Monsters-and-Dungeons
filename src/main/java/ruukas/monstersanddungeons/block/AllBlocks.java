0b318ad725886c2c60a18f51b2790a88bb83c4c7
Ruukas <lucas-olai@hotmail.com>
2017-11-16 00:26:03 +0100

    Added tentacles icon by KaijuRizard

--- src/main/java/ruukas/monstersanddungeons/block/AllBlocks.java
+++ src/main/java/ruukas/monstersanddungeons/block/AllBlocks.java
@@ -5,18 +5,24 @@ import java.util.HashSet;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent.Register;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.registries.IForgeRegistry;
 import ruukas.monstersanddungeons.MonstersAndDungeons;
 
 public class AllBlocks {    
-    public static HashSet<Block> blockSet = new HashSet<Block>();
-    public static HashSet<Block> itemBlockSet = new HashSet<Block>();
+    public static final HashSet<Block> blockSet = new HashSet<Block>();
+    public static final HashSet<Block> itemBlockSet = new HashSet<Block>();
+    public static final HashSet<Item> itemBlockItemsSet = new HashSet<Item>(); 
     
     public static Block HELMET;
     public static Block STATUE;
@@ -76,8 +82,17 @@ public class AllBlocks {
     		IForgeRegistry<Item> registry = event.getRegistry();
     		
     		for (Block block : itemBlockSet){
-    			registry.register(new ItemBlock(block).setCreativeTab(CreativeTabs.BUILDING_BLOCKS).setRegistryName(Block.REGISTRY.getNameForObject(block)));
-    			System.out.println(Block.REGISTRY.getNameForObject(block));
+    			Item item = new ItemBlock(block).setCreativeTab(CreativeTabs.BUILDING_BLOCKS).setRegistryName(block.getRegistryName().toString());
+    			registry.register(item);
+    			itemBlockItemsSet.add(item);
+    		}
+    	}
+    	
+    	@SideOnly(Side.CLIENT)
+    	@SubscribeEvent
+    	public static void registerItemModels(ModelRegistryEvent event){
+    		for (Item item : itemBlockItemsSet){
+        		ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName().toString(), "inventory"));
     		}
     	}
     }
